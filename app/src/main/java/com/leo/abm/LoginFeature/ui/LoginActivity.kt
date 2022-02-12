b063904826beedc1e05dcd4593ee94ec65917edf
leonelgomez1990 <leonelgomez@live.com.ar>
2022-02-11 23:01:22 -0300

    BASE
    new structure for loginfeature

--- app/src/main/java/com/leo/abm/LoginFeature/ui/LoginActivity.kt
+++ app/src/main/java/com/leo/abm/LoginFeature/ui/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.leo.abm
+package com.leo.abm.LoginFeature.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.leo.abm.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
---
+++ app/src/main/java/com/leo/abm/LoginFeature/ui/viewmodels/SplashViewModel.kt
@@ -0,0 +1,7 @@
+package com.leo.abm.LoginFeature.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class SplashViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
